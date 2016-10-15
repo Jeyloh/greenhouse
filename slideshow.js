170399b782955e62b72c3703bcb20ef0672830b4
Jørgen L. Hansen <Jrgen L. Hansen>
2016-10-15 17:23:05 +0100

    Header template fixed, tweaking for interactive mobile design and added features

--- slideshow.js
+++ slideshow.js
@@ -34,9 +34,9 @@ function showSlides() {
         slides[i].style.display = "none"; 
     }
     slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
+    if (slideIndex > slides.length) {slideIndex = 1;} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 /* Non automatic slides:
