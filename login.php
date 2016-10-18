29d3e6d8a0af7aeb9b961fe4630b4d0ded318381
unknown <20074232@IT101PC06.wit.ie>
2016-10-18 13:13:22 +0100

    more responsiveness work and added logo, headers etc

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>The Greenhouse - Login</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="login.css">
+	<script src="jquery-3.1.1.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		// Jquery code here
+	});
+	</script>
+
+</head>
+<body>
+
+	<div id="outer-image">
+		<!-- Add tabs here for either Creating user or logging in -->
+		<div id="wrapper">
+			<main>
+				<div id="loginform">
+				<!-- INSERT LOGIN FORM HERE -->
+					<h3>Login</h3>
+					<form method="GET" action="membership_insert_record.php">
+						<label>Username </label><br>
+						<input type=text name=loginusername size=30><br>
+						<label>Password</label><br>
+						<input type=password name=loginpassword size=30><br>
+						<input type=submit value=Login><br>
+					</form>
+				</div>
+			</main>
+		</div>
+
+
+	</div>
+</body>
+</html>
