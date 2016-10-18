41a09c27d3c041cbcdb376d3a13683ef5a5d8658
Jørgen L. Hansen <Jrgen L. Hansen>
2016-10-18 23:56:24 +0100

    finalized header and started on js for mobilebutton

---
+++ js/mobile-menubutton.js
@@ -0,0 +1,11 @@
+
+
+// Toggle the navbar visibility from visible to hidden or opposite
+function toggleNavbar() {
+	var navBar = document.getElementsByTagName("NAV");
+	if (navBar.style.visibility = "visible") {
+		navBar.style.visibility = "hidden";
+	} else {
+		navBar.style.visibility = "visible";
+	}
+}
